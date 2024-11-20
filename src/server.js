b80e9afb0c89f588ef822f2a5b702cada231c665
MiqueiasAllisson <miqueias.allisson@gmail.com>
2024-11-20 20:18:38 -0300

    foi alterado no server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
 import http from 'node:http';
 
+// JSON - JavaScript Object Notation
+
+const users = []
+
 const server = http.createServer((req, res) => {
     const {method, url} = req;
-    console.log(method, url)
+    if(method === 'GET' && url === '/users' ) {
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+    if (method === 'POST' && url === '/users') {
+        users.push({
+            id: 1,
+            name: 'Jonh Doe',
+            email: "johndoe@example.com"
+        })
+        return res.end("Criação de usuários")
+    }
        return res.end("Hello Worldd")
 })
 
